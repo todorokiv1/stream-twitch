7f76149be7d00f34167408369be42118a6d2dd13
todorokiv1 <155774992+todorokiv1@users.noreply.github.com>
2024-05-05 22:42:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,41 +2,41 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Self Bot Rich Presence working');
+  res.send('Self Bot Rich Presence working');
 });
 app.listen(process.env.PORT, () => {
-    console.log('Self Bot Rich Presence working on port ' + process.env.PORT);
+  console.log('Self Bot Rich Presence working on port ' + process.env.PORT);
 });
 
 const Discord = require('discord.js-selfbot-v13');
 const client = new Discord.Client({
-    checkUpdate: false
+  checkUpdate: false
 });
 
 client.on('ready', async () => {
 
-    const rpc = new Discord.RichPresence()
-        .setApplicationId('534203414247112723')
-        .setType('STREAMING')
-        .setURL(`https://www.youtube.com/watch?v=HTp5PH8ot6Q&list=RDGMEMHDXYb1_DDSgDsobPsOFxpA&ab_channel=NightLovell`)
-        .setDetails(`shadows of evil`)
-        .setName(`shadows of evil`)
-        .setState(`Ад, shadows of evil`)
-        .setParty({
-            max: 5001,
-            current: 4690,
-            id: Discord.getUUID(),
-        })
-        .setAssetsLargeImage(`mp:attachments/754377615774842923/1083781876352819200/ce050454b14baa8a677c9fe3940bd094.jpg`)
-        .setAssetsLargeText(`dead`)
-        .setAssetsSmallImage(`mp:attachments/754377615774842923/1083782130930303126/e1a431f195998ae2c25ad1fc30f497c3.jpg`)
-        .setAssetsSmallText(`decay`)
-        .addButton(`rot`, `https://crosshead.tk/`)
-        .addButton(`decay`, `https://feds.lol/grim`);
-    client.user.setActivity(rpc.toJSON());
+  const rpc = new Discord.RichPresence()
+    .setApplicationId("1043168841326403594")
+    .setType('STREAMING')
+    .setURL(`https://www.youtube.com/watch?v=HTp5PH8ot6Q&list=RDGMEMHDXYb1_DDSgDsobPsOFxpA&ab_channel=NightLovell`)
+    .setDetails(`𝐁𝐄𝐃 𝐓𝐈𝐌𝐄`)
+    .setName(`𝐓𝐎𝐃𝐎𝐑𝐎𝐊𝐈`)
+    .setState(`𝐃𝐫𝐞𝐚𝐦 𝐖𝐨𝐫𝐥𝐝......`)
+    .setParty({
+      max: 5000,
+      current: 1111,
+      id: Discord.getUUID(),
+    })
+    .setAssetsLargeImage('https://media.discordapp.net/attachments/1108081101328629823/1236763975769784390/anime-anime-girl-sleep.gif?ex=663931b5&is=6637e035&hm=d6543ee0dfbcac2a51cb9a9f7a96e32bb96b186506956ae768c33c0c647decd2&')
+    .setAssetsLargeText(`𝐃𝐄𝐀𝐃`)
+    .setAssetsSmallImage(`https://media.discordapp.net/attachments/1108081101328629823/1236764044174561381/image0.gif?ex=663931c6&is=6637e046&hm=7c8b64867caebf7f68473a2cf0134523716bef2f8207fc6aff770b1ff1bf5ff6&`)
+    .setAssetsSmallText(`decay`)
+    .addButton(`すべてマイクな`, `https://youtu.be/zYNhoj-3H8E?si=V5P8IU9mJoIm8Uku`)
+    .addButton(`すべてマイク`, `https://youtu.be/hH8jxbaSdQk`);
+  client.user.setActivity(rpc.toJSON());
 
-    console.log(`${client.user.tag} is ready!`);
+  console.log(`${client.user.tag} is ready!`);
 });
 
 //Account Token
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
